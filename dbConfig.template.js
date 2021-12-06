64a5ce69b0da7417090e5c58e954df602e555a96
Haakon Willem Hoel Bakker <haakon.hoel.bakker@vipps.no>
2021-12-06 13:30:31 +0100

    Yolo on dbConfig. Testing it, but need to figure out a better way to handle config

--- dbConfig.template.js
+++ dbConfig.template.js
@@ -1,10 +1,9 @@
 // these keys, or whatever config you need for your db
 const dbConfig = {
-  endpoint: '<COSMOS ENDPOINT>',
-  key: '<COSMOS KEY>',
-  databaseId: '<COSMOS Database ID>',
-  containerId: '<COSMOS Container ID>',
-  partitionKey: { kind: 'ash', paths: ['id'] } // or whatever your partition key is
+    endpoint: 'https://cosmos-ots-test.documents.azure.com:443/',
+    databaseId: 'db-ots',
+    containerId: 'ots',
+    partitionKey: { kind: 'ash', paths: ['id'] } // or whatever your partition key is
 };
 
 module.exports = dbConfig;
